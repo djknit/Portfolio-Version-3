ffddb35b5f5ea9e6721db6cc19db0af51cdda35c
djknit <djknit@gmail.com>
2018-10-13 22:50:14 -0500

    adjustments

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,14 +9,14 @@ $(".port_link").hover(function() {
     const deployedLink = $(`<a class="port_icon_label deployed-link" id="${projectName}-deployed-link" href="https://djknit.github.io/${projectName}">Deployed Page</a>`);
     // Create a label to link to the repo and append it to the icon link
     const repoLink = $(`<a class="port_icon_label repo-link" id="${projectName}-repo-link" href="https://github.com/djknit/${projectName}">Repository</a>`);
-    // After .1 seconds, append the links to the icon
+    // Append the links to the icon
     $(this).append(deployedLink).append(repoLink);
 // When mouse leaves icon...
 }, function() {
     // Grab the project-name attribute from the link that was hovered over
     const projectName = $(this).attr("project-name");
     // Move the title label back to its original position
-    $("#" + projectName + "-label").attr("style", "top: 65.9%;");
+    $("#" + projectName + "-label").attr("style", "animation-name: moveLabelBack; animation-duration: 300ms; top: 65.9%;");
     // Remove the links to the repo and the live page
     $("#" + projectName + "-repo-link").remove();
     $("#" + projectName + "-deployed-link").remove();
