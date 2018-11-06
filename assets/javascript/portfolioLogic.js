6ec35099c8331ba06846ae3d931621bca422f6fd
djknit <djknit@gmail.com>
2018-11-06 16:22:56 -0600

    bamazon

--- assets/javascript/portfolioLogic.js
+++ assets/javascript/portfolioLogic.js
@@ -5,12 +5,17 @@ $(".port_link").hover(function() {
     const projectName = $(this).attr("project-name");
     // Grab the corresponding project title label and move it to the top of the icon
     $("#" + projectName + "-label").attr("style", "animation-name: moveLabel; animation-duration: 300ms; top: 0;");
-    // Create a label to link to the deployed page
-    const deployedLink = $(`<a class="port_icon_label deployed-link" id="${projectName}-deployed-link" href="https://djknit.github.io/${projectName}">Deployed Page</a>`);
+    // If the project has a deployed page...
+    if (projectName !== "liri-node-app" && projectName !== "bamazon") {
+        // Create a label to link to the deployed page
+        const deployedLink = $(`<a class="port_icon_label deployed-link" id="${projectName}-deployed-link" href="https://djknit.github.io/${projectName}">Deployed Page</a>`);
+        // Append the deployed page link to the icon
+        $(this).append(deployedLink);
+    }
     // Create a label to link to the repo and append it to the icon link
     const repoLink = $(`<a class="port_icon_label repo-link" id="${projectName}-repo-link" href="https://github.com/djknit/${projectName}">Repository</a>`);
-    // Append the links to the icon
-    $(this).append(deployedLink).append(repoLink);
+    // Append the repo link to the icon
+    $(this).append(repoLink);
     // Hide the links for .1 seconds so they cannot be clicked at the same time they are appearing (mobile bug)
     deployedLink.attr("style", "display: none;");
     repoLink.attr("style", "display: none;");
